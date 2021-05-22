20e8c045a28add8139ad156f7a7fca6ba146f2bd
Phil Winder <phil@WinderResearch.com>
2021-05-22 12:21:56 +0100

    test: basic example should test current files

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,3 +1,3 @@
 module "jupyter" {
-  source = "combinator-ml/jupyter/k8s"
+  source = "../.."
 }
--- jupyter.tf
+++ jupyter.tf
@@ -1,11 +1,3 @@
-# Try to avoid hard coding any variables. They should be vars or locals.
-
-resource "kubernetes_namespace" "namespace" {
-  metadata {
-    name = var.namespace
-  }
-}
-
 resource "kubernetes_deployment" "jupyter" {
   metadata {
     name      = local.deployment_name
@@ -33,10 +25,10 @@ resource "kubernetes_deployment" "jupyter" {
         container {
           image = var.image
           name  = local.name
-        }
-        port {
-          name           = "http"
-          container_port = 8888
+          port {
+            name           = "http"
+            container_port = 8888
+          }
         }
       }
     }
